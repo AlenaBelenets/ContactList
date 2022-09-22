f90a58a87a0efb70e77715f7e718da9876ac810a
AlenaBelenets <alenaharenkova@gmail.com>
2022-09-22 18:19:09 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Alena Belenets on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
